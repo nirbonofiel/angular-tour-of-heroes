639d3f119826540b82bdda18ae67d0de69d7eaff
Nir Bonofiel <nirbono10@gmail.com>
2017-11-29 17:38:56 +0200

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import { InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { HeroesComponent }  from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard.component';
+
+import {AppRoutingModule} from './app-routing.module';
+import {HeroSearchComponent} from './hero-search.component';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroSearchComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
